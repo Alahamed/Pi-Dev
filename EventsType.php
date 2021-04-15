6adb9f9ec4317b67704a726b4e48ec983dd45cde
chadha-mouelhi <78917727+chadha-mouelhi@users.noreply.github.com>
2021-04-15 12:41:36 +0200

    Add files via upload

---
+++ EventsType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Events;
+use App\Entity\Categorie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nomEvent')
+            ->add('dateDeb')
+            ->add('dateFin')
+            ->add('nbr_place')
+            ->add('categorie', EntityType::class,
+            [
+                'class'=>Categorie::class,
+                'choice_label'=>'nomcategorie'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Events::class,
+        ]);
+    }
+}
