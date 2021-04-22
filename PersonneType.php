92bdf5859eff556bc4989ed75a788c9c798a763c
Alahamed <47696500+Alahamed@users.noreply.github.com>
2021-04-22 11:39:11 +0100

    Add files via upload

---
+++ PersonneType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Personne;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
+
+class PersonneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('mdp', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                    'label' => 'password',
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                ],
+                'invalid_message' => 'The password fields must match.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => true,
+            ])
+
+
+            ->add('dateNaissance')
+
+            ->add('telephone')
+
+            ->add('status', ChoiceType::class,[
+                'choices'=> array(
+                    'Membre'=>'Membre',
+                    'Admin'=>'Admin',
+                )
+            ])
+            ->add('image',FileType::class, [
+                'label'=>'Image',
+                'mapped'=> false
+            ])
+
+            ->add('etat', CheckboxType::class, [
+                'required' => false,            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Personne::class,
+        ]);
+    }
+}
